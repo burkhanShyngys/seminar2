539afe60b50d238d9b7f920ed717b80795914e01
burkhanShyngys <47245516+burkhanShyngys@users.noreply.github.com>
2019-02-15 22:03:15 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from mybox import MyBox
+from mybox import myBox
 from myclass import Calculator
 
-box = MyBox()
+box = myBox()
 box.add(Calculator(5, '+', 6))
 box.add(Calculator(12, '-', 4))
 box.add(Calculator(3, '*', 5))
 box.add(Calculator(6, '/', 4))
 
 box.remove(2)
-	for i in box:
-		i.print_ans()
+for i in box:
+	i.print_ans()
